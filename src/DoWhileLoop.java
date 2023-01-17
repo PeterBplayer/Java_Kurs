1f1570cbcf90b139913bf0c9caafd3f0a51b8a5c
Piotr Keppel <piotr.keppel@soflab.pl>
2023-01-17 10:26:14 +0100

    Added WhileLoop.java and DoWhileLoop.java

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,11 @@
+// Pętla do while - najpierw wykonywana jest instrukcja w bloku do, a następnie sprawdzany jest warunek
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int number = 5;
+        do {
+            System.out.println("Number value is " + number + " and it is less than 20.");
+            number++;
+        }
+        while (number < 20);
+    }
+}
