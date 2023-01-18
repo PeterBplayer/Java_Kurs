956038a0089fbd2ffdb3e3b1d9b4ecd16ee1aafa
Piotr Keppel <piotr.keppel@soflab.pl>
2023-01-18 15:01:36 +0100

    Added User.java and UserTest.java

---
+++ src/UserTest.java
@@ -0,0 +1,12 @@
+public class UserTest {
+    public static void main(String[] args) {
+//        User user = new User();
+
+        User user = new User("Kinga", "12345");
+
+       /* user.userName = "Piotr";
+        user.password = "QWERTY";*/
+
+        user.sayHello();
+    }
+}
