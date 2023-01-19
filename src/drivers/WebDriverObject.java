9f736a6aaa8349a02bd4e9ddb6261bd4b63b74db
Piotr Keppel <piotr.keppel@soflab.pl>
2023-01-19 17:15:08 +0100

    Changes in WebDriverObject.java

--- src/drivers/WebDriverObject.java
+++ src/drivers/WebDriverObject.java
@@ -1,13 +1,23 @@
 package drivers;
 
 public class WebDriverObject {
-    public static void main(String[] args) {
-        ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get();
-        chromeDriver.findElementBy();
-
-        FirefoxDriver firefoxDriver = new FirefoxDriver();
-        firefoxDriver.get();
-        firefoxDriver.findElementBy();
+    private static WebDriver getDriver(String name) {
+        if(name.equals("chrome")){
+            return new ChromeDriver();
+        } else if (name.equals("firefox")) {
+            return new FirefoxDriver();
+        }
+        return null;
     }
+    public static void main(String[] args) {
+        WebDriver driver = getDriver("chrome");
+        driver.get();
+        driver.findElementBy();
+
+       /* FirefoxDriver firefoxDriver = new FirefoxDriver();
+        firefoxDriver.get();
+        firefoxDriver.findElementBy();*/
+    }
+
+
 }
