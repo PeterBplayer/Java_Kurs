72ae49d6dcc56ba8e5e433fe82931d9f77dc6dca
Piotr Keppel <piotr.keppel@soflab.pl>
2023-01-20 15:43:45 +0100

    Added DriverType.java ENUM, refactoring WebDriverObject.java class

--- src/drivers/WebDriverObject.java
+++ src/drivers/WebDriverObject.java
@@ -1,23 +1,27 @@
 package drivers;
 
 public class WebDriverObject {
-    private static WebDriver getDriver(String name) throws NoValidBrowserName {
-        if (name.equals("chrome")) {
+    private static WebDriver getDriver(DriverType type) {
+        if (type.name.equals("chrome")) {
+            System.out.println(type.path);
             return new ChromeDriver();
-        } else if (name.equals("firefox")) {
-            return new FirefoxDriver();
         }
-        throw new NoValidBrowserName("No valid browser name.");
+        System.out.println(type.path);
+        return new FirefoxDriver();
     }
 
-    public static void main(String[] args) throws NoValidBrowserName {
-        WebDriver driver = getDriver("safari");
+    public static void main(String[] args) {
+
+        DriverType[] driverTypes =  DriverType.values();
+        for (int i = 0; i < driverTypes.length; i++) {
+            System.out.println(driverTypes[i].name);
+            System.out.println(driverTypes[i].path);
+        }
+
+        WebDriver driver = getDriver(DriverType.FIREFOX);
         driver.get();
         driver.findElementBy();
 
-       /* FirefoxDriver firefoxDriver = new FirefoxDriver();
-        firefoxDriver.get();
-        firefoxDriver.findElementBy();*/
     }
 
 
