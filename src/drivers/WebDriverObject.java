8ef2633d6d35f53244016993c8c6a3ac3f235ef5
Piotr Keppel <piotr.keppel@soflab.pl>
2023-01-20 12:56:05 +0100

    Added in WebDriverObject.java exception handling, adding NoValidBrowserName.java custom exception

--- src/drivers/WebDriverObject.java
+++ src/drivers/WebDriverObject.java
@@ -1,16 +1,17 @@
 package drivers;
 
 public class WebDriverObject {
-    private static WebDriver getDriver(String name) {
-        if(name.equals("chrome")){
+    private static WebDriver getDriver(String name) throws NoValidBrowserName {
+        if (name.equals("chrome")) {
             return new ChromeDriver();
         } else if (name.equals("firefox")) {
             return new FirefoxDriver();
         }
-        return null;
+        throw new NoValidBrowserName("No valid browser name.");
     }
-    public static void main(String[] args) {
-        WebDriver driver = getDriver("chrome");
+
+    public static void main(String[] args) throws NoValidBrowserName {
+        WebDriver driver = getDriver("safari");
         driver.get();
         driver.findElementBy();
 
