0ae390a66cb0b833f5f0aed5e6bcca4059f192e1
Piotr Keppel <piotr.keppel@soflab.pl>
2023-01-26 13:22:17 +0100

    Added Substring.java class

---
+++ src/Substring.java
@@ -0,0 +1,8 @@
+public class Substring {
+
+    public static void main(String[] args) {
+        String string = "username";
+        String substring = string.substring(string.length()-4);
+        System.out.println(substring);
+    }
+}
