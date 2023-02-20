d34a118254a4a556eb3da6248114fb16e9585894
Piotr Keppel <piotr.keppel@soflab.pl>
2023-02-20 14:47:57 +0100

    Added comment

--- src/dziedziczenie/offsprings/Footballer.java
+++ src/dziedziczenie/offsprings/Footballer.java
@@ -7,7 +7,7 @@ public class Footballer extends Person {
     public String footballClub;
 
     public Footballer(String name, int age, String footballClub) {
-        super(name, age);
+        super(name, age); // metoda super wywołuje konstruktor klasy nadrzędnej
         System.out.println("Jestem w konstruktorze FOOTBALLER");
         this.footballClub = footballClub;
     }
